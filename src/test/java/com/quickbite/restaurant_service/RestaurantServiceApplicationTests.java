b4bb7fbca5616bd5605dd61f664465a5b583c2db
Nam Dinh Thanh <thanhnamak@gmail.com>
2026-07-01 01:12:13 +0700

    update

--- src/test/java/com/quickbite/restaurant_service/RestaurantServiceApplicationTests.java
+++ src/test/java/com/quickbite/restaurant_service/RestaurantServiceApplicationTests.java
@@ -1,10 +1,16 @@
 package com.quickbite.restaurant_service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantServiceApplicationTests {
+	@Test
+	@Disabled("Tạm thời bỏ qua test context vì chưa cấu hình DB trên CI")
+	void contextLoads() {
+	}
+
 	@Test
 	void test() {
 		assert true;
