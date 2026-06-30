b312c98e67bd4f3d790cea1f49f2c33a0b643bb1
Nam Dinh Thanh <thanhnamak@gmail.com>
2026-07-01 01:04:12 +0700

    Revert "update"
    This reverts commit eab9aa6ef15a65c4c944c692560bb82f3014ccd7.

--- src/test/java/com/quickbite/restaurant_service/RestaurantServiceApplicationTests.java
+++ src/test/java/com/quickbite/restaurant_service/RestaurantServiceApplicationTests.java
@@ -5,6 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
 	@Test
 	void test() {
 		assert true;
